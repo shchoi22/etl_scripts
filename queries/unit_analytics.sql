9467bcd0b6902ca400f0537279b9a642623e5eae
Stan Choi <shchoi22@gmail.com>
2014-06-09 17:00:48 -0500

    updated unit analytics query

--- queries/unit_analytics.sql
+++ queries/unit_analytics.sql
@@ -12,5 +12,12 @@
   ,availabledate as available_date
   ,cast(bedrooms as int) as bedrooms
   ,cast(bathrooms as int) as bathrooms
-
+  ,case when unitamenities not like '%Not Available%'
+       and unitabbreviation not like '%Neighborhood%'
+       and status !='Occupied'
+       and cast(availabledate as date) between '1900-01-02' and 'today'
+       and monthlyrent != '0.00'
+       and type not in('RETAIL','COMM','Office')
+       and type not like '%RETAIL%' and type not like '%COMM%'
+       and subsidy = '--' then 'Yes' else 'No' end as ready_to_show
 from pw_unit
